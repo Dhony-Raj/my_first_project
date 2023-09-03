7f8218d0867a9466af8b89d73ab39e58f4aaab1b
Dhony Raj <Dhony-Raj>
2023-09-03 23:03:57 +0600

    model done

--- fms/models.py
+++ fms/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-from django.contrib.auth import get_user_model
 
-class userprofile(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    phone_number1 = PhoneNumberField(blank=True, null=True)
-    phone_number2 = PhoneNumberField(blank=True, null=True)
-    number_of_members = models.IntegerField()
+class Renter(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number1 = models.CharField(max_length=15)
+    phone_number2 = models.CharField(max_length=15) 
+    email = models.EmailField()
+    number_of_members = models.PositiveIntegerField()
     nid_number = models.CharField(max_length=20)
-    User = get_user_model()
     user_name = models.CharField(max_length=50, unique=True)
-    user = User.objects.create_user(user_name, password='strong_password')
+    password = models.CharField(max_length=100) 
+
+    def __str__(self):
+        return self.name
